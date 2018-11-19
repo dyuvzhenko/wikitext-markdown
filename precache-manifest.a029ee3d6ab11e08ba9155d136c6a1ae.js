5af5c563cf4f3a729dccdceaa0238f9b03c82fc2
Denis Iuvzhenko <yuvzhenko@inbox.ru>
2018-11-19 19:12:29 +0800

    demo app

---
+++ precache-manifest.a029ee3d6ab11e08ba9155d136c6a1ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4694b5a9fb4f4b6a7d30",
+    "url": "/static/js/main.4694b5a9.chunk.js"
+  },
+  {
+    "revision": "dc6f685ac7b82ab0d464",
+    "url": "/static/js/1.dc6f685a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e217ecbb2c5a7551aadf08d852aa01b",
+    "url": "/index.html"
+  }
+];
