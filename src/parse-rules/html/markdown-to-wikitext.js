8b280fe898d56882b0848c73c7b1bdaa00aedffd
Denis Iuvzhenko <yuvzhenko@inbox.ru>
2018-11-22 18:42:41 +0800

    keywords for package.json

--- src/parse-rules/html/markdown-to-wikitext.js
+++ src/parse-rules/html/markdown-to-wikitext.js
@@ -1,20 +1,4 @@
 // html/markdown-to-wikitext.js
-function htmlHeadings(line) {}
-
-function htmlUnorderedList(line) {}
-
-function htmlOrderedList(line) {}
-
-function htmlCode(line) {}
-
-function htmlBoldAndItalic(line) {}
-
-function htmlBlockquoteFirstPass(line) {}
-
-function htmlBlockquoteSecondPass(line) {}
-
-function htmlLink(line) {}
-
 function htmlBold(line) {
   return line.replace(/<b>(.*)<\/b>/g, `'''$1'''`)
 }
@@ -24,14 +8,6 @@ function htmlItalic(line) {
 }
 
 module.exports = [
-  // htmlHeadings,
-  // htmlUnorderedList,
-  // htmlOrderedList,
-  // htmlCode,
-  // htmlBoldAndItalic,
-  // htmlBlockquoteFirstPass,
-  // htmlBlockquoteSecondPass,
-  // htmlLink,
   htmlBold,
   htmlItalic
 ]
--- src/parse-rules/html/wikitext-to-markdown.js
+++ src/parse-rules/html/wikitext-to-markdown.js
@@ -1,22 +1,4 @@
 // html/wikitext-to-markdown.js
-function htmlOrderedList(line) {}
-
-function htmlSetSequenceForOrderedList(line) {}
-
-function htmlHeadings(line) {}
-
-function htmlUnorderedList(line) {}
-
-function htmlCode(line) {}
-
-function htmlBoldAndItalic(line) {}
-
-function htmlBlockquote(line, index, arr) {}
-
-function htmlRemoveSpecialSymbols(line) {}
-
-function htmlLink(line) {}
-
 function htmlBold(line) {
   return line.replace(/<b>(.*)<\/b>/g, `**$1**`)
 }
@@ -26,15 +8,6 @@ function htmlItalic(line) {
 }
 
 module.exports = [
-  // htmlOrderedList,
-  // htmlSetSequenceForOrderedList,
-  // htmlHeadings,
-  // htmlUnorderedList,
-  // htmlCode,
-  // htmlBoldAndItalic,
-  // htmlBlockquote,
-  // htmlRemoveSpecialSymbols,
-  // htmlLink,
   htmlBold,
   htmlItalic
 ]
